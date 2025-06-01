984938f64f642d0e8ba92b7397ac7e326096423d
Prince Bhesaniya <126847549+PrinceBhesaniya28@users.noreply.github.com>
2025-06-01 20:48:41 +0530

    about us page completely update

--- app/components/aboutUs/page.tsx
+++ app/components/aboutUs/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function AboutUs() {
   return (
     <section className="w-full min-h-screen lg:min-h-[554px] bg-[#EFF0F1] flex flex-col items-center py-16">
-      <div className="w-full max-w-4xl px-4">
+      <div className="w-full max-w-4xl px-4 lg:pl-16">
         {/* Heading and Bullets */}
         <div className="mb-10">
           <h2 className="text-3xl font-normal text-black mb-4 text-left">Our Value Proposition</h2>
@@ -16,7 +16,7 @@ export default function AboutUs() {
           </ul>
         </div>
         {/* Value Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-[8px] mx-auto max-w-fit bg-transparent">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[8px] max-w-fit bg-transparent">
           {/* Card 1 */}
           <div className="bg-[#D3D5D866] hover:bg-[#97B7E833] transition-colors p-8 h-[100px] w-[416px] flex flex-col justify-center border-l-[3px] border-l-black">
             <span className="text-[22px] font-bold text-black mb-1">1,000+</span>
